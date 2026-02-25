47aef4807e554dbee7a6c32c9b36e65dae489daa
lokisassone-rgb <lokisassone@gmail.com>
2026-02-24 18:49:20 -0800

    update floorplan and pin assignments

--- ConvVerilog/design/floorplan/outputs/floorplan.tcl
+++ ConvVerilog/design/floorplan/outputs/floorplan.tcl
@@ -13,7 +13,7 @@
 # rectangular chip with a height that is twice the width.
 
 set core_aspect_ratio   1.00; # Aspect ratio 1.0 for a square chip
-set core_density_target 0.60; # Placement density of 70% is reasonable
+set core_density_target 0.55; # Placement density of 70% is reasonable
 
 # Make room in the floorplan for the core power ring
 
@@ -48,11 +48,24 @@ setFlipping s
 
 # Use automatic floorplan synthesis to pack macros (e.g., SRAMs) together
 
-planDesign
+#planDesign
 
-#placeInstance sram 55.4 65.4
+placeInstance ifmap_double_buffer_inst/ram/genblk1_width_macro_0__depth_macro_0__sram 38.835 1879.165 R90
+placeInstance ifmap_double_buffer_inst/ram/genblk1_width_macro_0__depth_macro_1__sram 776.815 1870.995 R90
 
-addHaloToBlock 10.88 10.88 10.88 10.88 sram
+placeInstance weight_double_buffer_inst/ram/genblk1_width_macro_0__depth_macro_0__sram 1868.39 1875.52 R0
+placeInstance weight_double_buffer_inst/ram/genblk1_width_macro_0__depth_macro_1__sram 1875.795 1163.65 R0
+
+placeInstance ofmap_buffer_inst/ram0/genblk1_width_macro_0__sram 2100.475 629.89 R90
+placeInstance ofmap_buffer_inst/ram0/genblk1_width_macro_1__sram 2101.855 69.95 R90
+placeInstance ofmap_buffer_inst/ram0/genblk1_width_macro_2__sram 1060.09 52.94 R90
+placeInstance ofmap_buffer_inst/ram0/genblk1_width_macro_3__sram 1534.12 51.365 R90
+placeInstance ofmap_buffer_inst/ram1/genblk1_width_macro_0__sram 70.24 1190.465 R90
+placeInstance ofmap_buffer_inst/ram1/genblk1_width_macro_1__sram 59.56 648.91 R90
+placeInstance ofmap_buffer_inst/ram1/genblk1_width_macro_2__sram 78.66 58.58 R90
+placeInstance ofmap_buffer_inst/ram1/genblk1_width_macro_3__sram 588.325 52.235 R90
+
+addHaloToBlock 5 5 5 5 -allBlock
 
 # Create a routing blockage on li1 layer over the entire chip area, so that 
 # Innovus does not use this layer for routing
