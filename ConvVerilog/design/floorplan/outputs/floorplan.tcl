57606ac3580e3e495c674f416f8a4d4c0f733bf7
lokisassone-rgb <lokisassone@gmail.com>
2026-02-25 18:41:43 -0800

    update code

--- ConvVerilog/design/floorplan/outputs/floorplan.tcl
+++ ConvVerilog/design/floorplan/outputs/floorplan.tcl
@@ -70,4 +70,4 @@ addHaloToBlock 5 5 5 5 -allBlock
 # Create a routing blockage on li1 layer over the entire chip area, so that 
 # Innovus does not use this layer for routing
 
-#createRouteBlk -box {0 0 614 614} -layer {li1}
+createRouteBlk -box {0 0 2606 2604} -layer {li1}
