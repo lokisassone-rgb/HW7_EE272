cd91a6b0ea4e587e7a4be17d0346aaabc81b4b0a
lokisassone-rgb <lokisassone@gmail.com>
2026-02-27 00:57:17 -0800

    try out design Loki

--- ConvVerilog/design/pin-placement/outputs/pin-assignments.tcl
+++ ConvVerilog/design/pin-placement/outputs/pin-assignments.tcl
@@ -3,8 +3,8 @@
 #=========================================================================
 # Pin assignments for the conv accelerator.
 #   - Top:    ofmap data + ofmap handshake
-#   - Left:   clk, rst_n
-#   - Bottom: ifmap/weight data + handshake, config data + handshake
+#   - Left:   clk, rst_n, ifmap/weight handshake, config handshake
+#   - Bottom: ifmap/weight data, config data
 #   - Right:  unused
 #
 # Metal layers chosen to match edge orientation:
@@ -26,30 +26,22 @@ for {set i 31} {$i >= 0} {incr i -1} {
 }
 lappend pins_top ofmap_vld ofmap_rdy
 
-# Left side: clk and rst_n spread evenly (met3, horizontal metal for left edge)
-# Compute die height and place pins at 1/3 and 2/3 of the left edge
+# Left side: clk, rst_n, handshake signals spread evenly (met3, horizontal metal)
 
 set pins_left {}
 lappend pins_left clk rst_n
+lappend pins_left ifmap_weight_vld ifmap_weight_rdy
+lappend pins_left config_vld config_rdy
 
-set die_box [dbGet top.fPlan.box]
-set die_lly [lindex [lindex $die_box 0] 1]
-set die_ury [lindex [lindex $die_box 0] 3]
-set die_height [expr $die_ury - $die_lly]
-set left_range_start [expr $die_lly + $die_height / 3.0]
-set left_range_end   [expr $die_lly + 2.0 * $die_height / 3.0]
-
-# Bottom side: ifmap/weight data + handshake, config data + handshake (met2, vertical metal)
+# Bottom side: ifmap/weight data, config data (met2, vertical metal)
 
 set pins_bottom {}
 for {set i 16} {$i >= 0} {incr i -1} {
   lappend pins_bottom "ifmap_weight_data\[$i\]"
 }
-lappend pins_bottom ifmap_weight_vld ifmap_weight_rdy
 for {set i 15} {$i >= 0} {incr i -1} {
   lappend pins_bottom "config_data\[$i\]"
 }
-lappend pins_bottom config_vld config_rdy
 
 # Right side: empty
 
@@ -59,5 +51,5 @@ set pins_right {}
 # Spread the pins evenly along the sides of the block
 
 editPin -layer met2 -pin $pins_top    -side TOP    -spreadType SIDE
-editPin -layer met3 -pin $pins_left   -side LEFT   -spreadType RANGE -spreadDirection clockwise -offsetStart $left_range_start -offsetEnd $left_range_end
+editPin -layer met3 -pin $pins_left   -side LEFT   -spreadType SIDE
 editPin -layer met2 -pin $pins_bottom -side BOTTOM -spreadType SIDE
